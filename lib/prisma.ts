f5e9e4f7e5c2532104acc3edf4df3a1744296c4a
Sebastian Siedler <55579075+SebastianSiedler@users.noreply.github.com>
2022-03-31 00:37:42 +0200

    log middleware time prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -21,3 +21,17 @@ export const prisma: PrismaClient =
 if (process.env.NODE_ENV !== 'production') {
   prismaGlobal.prisma = prisma;
 }
+
+prisma.$use(async (params, next) => {
+  const before = Date.now();
+
+  const result = await next(params);
+
+  const after = Date.now();
+
+  console.log(
+    `Query ${params.model}.${params.action} took ${after - before}ms`,
+  );
+
+  return result;
+});

8efddd8243ed01d79548452979559b8159f3f66f
Sebastian Siedler <55579075+SebastianSiedler@users.noreply.github.com>
2022-04-05 17:10:15 +0200

    new table view

--- src/pages/stats/index.tsx
+++ src/pages/stats/index.tsx
@@ -21,70 +21,87 @@ const StatsHome: NextPageWithAuthAndLayout = () => {
     },
   });
 
-  return (
-    <div>
-      {statsQuery.data?.map(({ saison, saison_stats }, i) => (
-        <div key={i}>
-          <div>{`Saison: ${saison.name}`}</div>
+  const [data, setData] = useState<ReturnType<typeof getTransformedStats>>([]);
 
-          {saison_stats.map((player) => (
-            <div
-              key={player.id}
-              style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                margin: '0.5rem 0 0.5rem 0',
-              }}
-            >
-              <div>{player.names[0]}</div>
-
-              {/* Stats */}
-              <div>
-                <div>Spiele: {player._count.spielereinsaetze}</div>
-                <div>
-                  Tore:{' '}
-                  {player.spielereinsaetze.reduce(
-                    (prev, curr) => (prev += curr.tore),
-                    0,
-                  )}
-                </div>
-                <div>
-                  Schulden:{' '}
-                  {player.spielereinsaetze.reduce(
-                    (prev, curr) => (prev += curr.bezahlt ? 0 : 5),
-                    0,
-                  )}
-                </div>
-              </div>
-
-              {/* Schulden begleichen */}
-              {player.spielereinsaetze.reduce(
+  const getTransformedStats = () => {
+    return (
+      statsQuery.data?.map(({ saison, saison_stats }) => {
+        return {
+          saison,
+          saison_stats: saison_stats
+            .map((player) => {
+              const schulden = player.spielereinsaetze.reduce(
                 (prev, curr) => (prev += curr.bezahlt ? 0 : 5),
                 0,
-              ) > 0 ? (
-                <Button
-                  onClick={() => {
-                    if (
-                      window.confirm(
-                        `Schulden von ${player.names[0]} wirklich begleichen?`,
-                      )
-                    ) {
-                      schuldenBegleichen.mutate({ spielerId: player.id });
-                    }
-                  }}
+              );
+              const anz_spiele = player._count.spielereinsaetze;
+
+              return {
+                schulden,
+                anz_spiele,
+                name: player.names[0],
+                ...player,
+              };
+            })
+            .sort((a, b) => b.schulden - a.schulden),
+        };
+      }) ?? []
+    );
+  };
+
+  useEffect(() => {
+    setData(getTransformedStats());
+  }, [statsQuery.data]);
+
+  return (
+    <>
+      {data.map(({ saison, saison_stats }, i) => (
+        <div key={i}>
+          <table className="relative w-full border">
+            <thead className="sticky top-0 bg-slate-300">
+              <div>{`Saison: ${saison.name}`}</div>
+              <tr>
+                <th>Name</th>
+                <th>Spiele</th>
+                <th>Schulden</th>
+              </tr>
+            </thead>
+            <tbody>
+              {saison_stats.map((player, i) => (
+                <tr
+                  key={player.id}
+                  className={
+                    (i % 2 == 0 ? 'bg-gray-100' : '') + ' min-h-[300px]'
+                  }
                 >
-                  Schulden begleichen
-                </Button>
-              ) : (
-                <div></div>
-              )}
-            </div>
-          ))}
+                  <td className="">{player.name}</td>
+                  <td className="text-center">{player.anz_spiele}</td>
+                  <td className="text-center">
+                    {player.schulden > 0 && (
+                      <Button
+                        onClick={() => {
+                          if (
+                            window.confirm(
+                              `Schulden von ${player.names[0]} wirklich begleichen?`,
+                            )
+                          ) {
+                            schuldenBegleichen.mutate({ spielerId: player.id });
+                          }
+                        }}
+                      >
+                        {`${player.schulden}€ begleichen?`}
+                      </Button>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       ))}
+
       <SignOut />
-    </div>
+    </>
   );
 };
 
@@ -96,6 +113,7 @@ StatsHome.getLayout = (page: React.ReactElement) => {
 export default StatsHome;
 
 import { signOut } from 'next-auth/react';
+import { useEffect, useState } from 'react';
 
 const SignOut: React.FC = () => {
   return <Button onClick={() => signOut()}>Sign out</Button>;
